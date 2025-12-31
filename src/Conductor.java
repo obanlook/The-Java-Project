38bfd8233e5c5d9661f38d4f0a80cf43d9140dee
ArashMAlMAl <150516345+ArashMAlMAl@users.noreply.github.com>
2025-12-31 01:47:04 +0100

    Delete src directory

--- src/Conductor.java
+++
@@ -1,5 +0,0 @@
-public class Conductor extends Character {
-    public Conductor() {
-        super("Conductor", new ServiceCoach());
-    }
-}
--- src/Language.java
+++
@@ -1,6 +0,0 @@
-public enum Language {
-    ENGLISH,
-    FRENCH,
-    SPANISH,
-    DUTCH
-}
